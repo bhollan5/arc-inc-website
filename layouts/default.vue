8dfae3f463b0f8a689f0ae6c31a2e10c35297efe
Ben Holland <bhollan5@kent.edu>
2019-12-17 12:59:57 -0500

    Committing repo

---
+++ layouts/default.vue
@@ -0,0 +1,62 @@
+<template>
+  <div>
+    <b-navbar>
+        <template slot="brand">
+            <b-navbar-item tag="router-link" :to="{ path: '/' }">
+                <img
+                    src="~assets/arc-logo.png"
+                    alt="Arc Inc. logo"
+                    height="100"
+                >
+            </b-navbar-item>
+        </template>
+
+        <template slot="end">
+          <b-navbar-dropdown label="SENIOR LIVING" hoverable>
+                <b-navbar-item href="#">
+                    Our Work
+                </b-navbar-item>
+                <b-navbar-item href="#">
+                    Services
+                </b-navbar-item>
+            </b-navbar-dropdown>
+            <b-navbar-dropdown label="HOME STAGING" hoverable>
+                <b-navbar-item href="#">
+                    Our Work
+                </b-navbar-item>
+                <b-navbar-item href="#">
+                    Services
+                </b-navbar-item>
+            </b-navbar-dropdown>
+            <b-navbar-item href="#">
+                US
+            </b-navbar-item>
+            <b-navbar-item href="#">
+                CONTACT
+            </b-navbar-item>
+        </template>
+    </b-navbar>
+
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      items: [
+        {
+          title: 'Home',
+          icon: 'home',
+          to: { name: 'index' }
+        },
+        {
+          title: 'Inspire',
+          icon: 'lightbulb',
+          to: { name: 'inspire' }
+        }
+      ]
+    }
+  }
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <section class="section">
+    <h1>hi plz show up</h1>
+    <div class="columns is-mobile">
+
+      <card
+        title="Modern"
+        icon="alert-decagram"
+      >
+        Built with <a href="https://vuejs.org/">
+          Vue.js
+        </a> and <a href="http://bulma.io/">
+          Bulma
+        </a>
+      </card>
+
+      <card
+        title="Lightweight"
+        icon="arrange-bring-to-front"
+      >
+        No other internal dependency
+      </card>
+    </div>
+  </section>
+</template>
+
+<script>
+import Card from '~/components/Card'
+
+export default {
+  name: 'HomePage',
+  layout: 'default',
+
+  components: {
+    Card
+  }
+}
+</script>
